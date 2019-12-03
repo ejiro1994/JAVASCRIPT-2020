ebfb0157b01cfb21b3b078c6a27ec69b804e8e13
ejiro <ejiro1994@gmail.com>
2019-12-03 16:34:27 +0000

    static js

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+const defaultResult = 0
+let currentResult = defaultResult
+
+
+currentResult = (currentResult + 10)
+
+let calculationDescription = `${defaultResult} + 10`
+
+outputResult(currentResult, calculationDescription)
